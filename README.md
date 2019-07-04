5259ab3c0ce38668746ec1cf23d22fff1dc4cf58
Jagannathan Tiruvallur Eachambadi <jagannathante@gmail.com>
2019-07-04 11:47:45 +0200

    make rudimentary script to download the latest comic

---
+++ README.md
@@ -0,0 +1,1 @@
+Download the latest comic from https://turnoff.us
---
+++ turnoff-dl.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+import feedparser
+from lxml import etree
+import urllib3
+
+URL = "https://turnoff.us/feed.xml"
+
+if __name__ == '__main__':
+    decoded_feed = feedparser.parse(URL)
+    title = decoded_feed.entries[0].title
+    summary = decoded_feed.entries[0].summary
+    image_link = etree.XML(summary).get('src')
+
+    http = urllib3.PoolManager()
+    response = http.request('GET', image_link, preload_content=False)
+
+    with open(f'%s.png' % title, 'wb') as f:
+        for chunk in response.stream(32):
+            f.write(chunk)
