b1463c41729a5805623054d727d4dfda9a4e0f26
Jagannathan Tiruvallur Eachambadi <jagannathante@gmail.com>
2019-07-04 11:50:24 +0200

    format with black

--- turnoff-dl.py
+++ turnoff-dl.py
@@ -5,15 +5,15 @@ import urllib3
 
 URL = "https://turnoff.us/feed.xml"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     decoded_feed = feedparser.parse(URL)
     title = decoded_feed.entries[0].title
     summary = decoded_feed.entries[0].summary
-    image_link = etree.XML(summary).get('src')
+    image_link = etree.XML(summary).get("src")
 
     http = urllib3.PoolManager()
-    response = http.request('GET', image_link, preload_content=False)
+    response = http.request("GET", image_link, preload_content=False)
 
-    with open(f'%s.png' % title, 'wb') as f:
+    with open(f"%s.png" % title, "wb") as f:
         for chunk in response.stream(32):
             f.write(chunk)
